42016d88d340b6c1f7903946fec6a79766542f2b
Ateka Sultana <atekaoishi78@gmail.com>
2023-09-25 21:41:30 +0600

    navbar created

---
+++ src/components/Header/Navbar/Logo.jsx
@@ -0,0 +1,10 @@
+
+const Logo = () => {
+    return (
+        <div>
+         <img className="w-15 h-14" src="https://i.ibb.co/C7MrcNV/Logo.png" alt="" />
+        </div>
+    );
+};
+
+export default Logo;
